cef0c59e034ec315917e046b855692463aa3e35b
Kristian Gutic <kristian.gutic@gmail.com>
2021-05-30 15:28:13 +0200

    basic impl

--- TaskManager/MainWindow.xaml.cs
+++ TaskManager/MainWindow.xaml.cs
@@ -1,13 +1,23 @@
-﻿namespace TaskManager
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TaskManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        // Demo purpose ! It is goind to be moved elsewhere
+        private List<Process> _processes = new List<Process>();
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _processes = Process.GetProcesses().ToList();
+            processList.ItemsSource = _processes;
         }
     }
 }
---
+++ TaskManager/Models/CustomProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Models
+{
+    /*
+     * Process data class encapsulating all necessary data.
+     * To gather information about processes we use System.Diagnostics .
+     */
+    public class CustomProcess
+    {
+        
+    }
+}
