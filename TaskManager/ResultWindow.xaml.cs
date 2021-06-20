623c6bfdf494a87ad39595ee5adecc7ffaae977b
Kristian Gutic <kristian.gutic@gmail.com>
2021-06-20 13:43:20 +0200

    update

--- TaskManager/ResultWindow.xaml.cs
+++ TaskManager/ResultWindow.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
+using LiveCharts;
+using LiveCharts.Wpf;
 using TaskManager.Utils;
+using System.Collections.Generic;
 
 namespace TaskManager
 {
     public partial class ResultWindow : Window
     {
-        private CustomProcess _customProcess;
+        public CustomProcess _customProcess { get; }
         
         public string CpuPercentage { get; }
         public string RamAverage { get; }
@@ -15,6 +19,8 @@ namespace TaskManager
         public string RamMax { get; }
         public string Duration { get; }
         
+        public SeriesCollection CpuCollection { get; set; }
+        public SeriesCollection RamCollection { get; set; }
         
         
         public ResultWindow(CustomProcess customProcess)
@@ -30,8 +36,52 @@ namespace TaskManager
             CPUAVERAGE.Text = $"CPU AVERAGE: {CpuPercentage}%";
             CPUMAX.Text = $"CPU MAX: {CpuMax}%";
             RAMAVERAGE.Text = $"RAM AVERAGE: {RamAverage}MB";
-            RAMMAX.Text = $"RAM MAX: {RAMMAX}MB";
+            RAMMAX.Text = $"RAM MAX: {RamMax}MB";
             DURATION.Text = $"DURATION: {Duration} seconds";
+
+            CpuCollection = new SeriesCollection
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<float>(),
+                    Title = "CPU",
+                    LineSmoothness = 1,
+                    LabelPoint = (ChartPoint p) => $"{p.Y}%",
+                }
+            };
+            
+            RamCollection = new SeriesCollection
+            {
+                new LineSeries()
+                {
+                    Values = new ChartValues<float>(),
+                    Title = "RAM",
+                    LineSmoothness = 1,
+                    LabelPoint = (ChartPoint p) => $"{p.Y}MB",
+                }
+            };
+
+            foreach (var entry in _customProcess.cpuPercentageUsage)
+            {
+                CpuCollection[0].Values.Add(entry);
+            }
+            
+            foreach (var entry in _customProcess.ramUsage)
+            {
+                RamCollection[0].Values.Add(entry);
+            }
+
+            var ylabel = new List<string>();
+            for (int i = 0; i < 101; i++)
+            {
+                ylabel.Add(i.ToString());
+            }
+
+            cpuY.Labels = ylabel;
+            
+            chartCpu.Series = CpuCollection;
+            chartRam.Series = RamCollection;
+
         }
     }
 }
